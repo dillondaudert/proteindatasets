b916532663534a496b4969161506fe9ccbc03d5f
Dillon Daudert <dillongdaudert@gmail.com>
2018-06-10 18:21:52 -0400

    Add cUR50 parser and dataset

--- parsers.py
+++ parsers.py
@@ -32,6 +32,32 @@ def cpdb_parser(record):
 
     return dssp_id, seq_len, seq, seq_phyche, seq_pssm, ss
 
+
+def cUR50_parser(record):
+    """
+    Parse a cUR50 tfrecord Record into a tuple of tensors
+    """
+
+    keys_to_features = {
+        "uniref_id": tf.VarLenFeature(tf.string),
+        "seq_len": tf.FixedLenFeature([], tf.int64),
+        "seq": tf.VarLenFeature(tf.string),
+        "seq_phyche": tf.VarLenFeature(tf.float32),
+        }
+
+    parsed = tf.parse_single_example(record, keys_to_features)
+
+    uniref_id = parsed["uniref_id"]
+    uniref_id = tf.cast(uniref_id, tf.string)
+    seq_len = parsed["seq_len"]
+    seq_len = tf.cast(seq_len, tf.int32)
+    seq = parsed["seq"]
+    seq = tf.cast(seq, tf.string)
+    seq_phyche = tf.sparse_tensor_to_dense(parsed["seq_phyche"])
+
+    return uniref_id, seq_len, seq, seq_phyche
+
+
 def cpdb_dataset(tfrecords):
     """
     Open a tfrecords file in the cpdb format, parse, and
@@ -40,4 +66,14 @@ def cpdb_dataset(tfrecords):
 
     dataset = tf.data.TFRecordDataset(tfrecords)
     dataset = dataset.map(lambda x: cpdb_parser(x))
+    return dataset
+
+
+def cUR50_dataset(tfrecords):
+    """
+    Open a tfrecords file, parse, and return a tf.data.Dataset object
+    """
+
+    dataset = tf.data.TFRecordDataset(tfrecords)
+    dataset = dataset.map(lambda x: cUR50_parser(x))
     return dataset

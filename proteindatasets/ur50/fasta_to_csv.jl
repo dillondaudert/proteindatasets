09c380cc345f778ef11509e80dc9359e6ead076a
Dillon Daudert <dillongdaudert@gmail.com>
2018-09-01 18:53:39 -0400

    Read single fasta file to convert to single csv

--- proteindatasets/ur50/fasta_to_csv.jl
+++ proteindatasets/ur50/fasta_to_csv.jl
@@ -6,27 +6,21 @@ using BioSequences
 
 
 df = DataFrame(id=[], seq=[])
-reader = FASTA.Reader(open("/home/dillon/data/uniref50/uniref50.fasta", "r"))
+reader = FASTA.Reader(open("/home/dillon/data/cUR50/cur50.fasta", "r"))
 count = 0
 for (i, record) in enumerate(reader)
-    if i < 30*10^6
-        continue
-    elseif i == 35*10^6
-        break
-    end
     try
         if FASTA.hasidentifier(record) && FASTA.hassequence(record)
             push!(df, [FASTA.identifier(record), FASTA.sequence(record)])
         end
         if i % 10^5 == 0
-            @printf("%d read\n", i)
+            print("$i read\n")
         end
     catch exc
         print("Exception encountered: ", exc)
         print(record)
-        gc()
     end
 end
 close(reader)
 print("Writing csv.")
-CSV.write("uniref50_7.csv", df)
+CSV.write("/home/dillon/data/cUR50/cur50.csv", df)
